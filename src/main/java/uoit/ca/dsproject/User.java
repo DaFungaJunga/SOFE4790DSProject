eeeb8f51570865494e45eb7852ba4f8e588f8188
DaFungaJunga <shomari.simpson@uoit.net>
2018-09-29 18:15:51 -0400

    Add files via upload

--- src/main/java/uoit/ca/dsproject/User.java
+++ src/main/java/uoit/ca/dsproject/User.java
@@ -3,26 +3,39 @@ package uoit.ca.dsproject;
 import java.util.ArrayList;
 
 public class User {
-    public static ArrayList<BlockChain> blockChains=null;
+    public static ArrayList<BlockChain> blockChains = null;
 
-    public static void startBlockChain(String task){
+    public static void startBlockChain(String task) {
         blockChains = new ArrayList<BlockChain>();
-        BlockChain newBlockChain= new BlockChain();
+        BlockChain newBlockChain = new BlockChain();
         newBlockChain.startBlock(task);
         blockChains.add(newBlockChain);
     }
+
     public static void addToBlockChain(String task, BlockChain newBlockChain) {
         newBlockChain.continueBlock(task);
     }
-    public static BlockChain getBlockChain(String hash){
-        for (BlockChain bc :blockChains){
-            if (hash==bc.getHash()){
+
+    public static BlockChain getBlockChain(String hash) {
+        for (BlockChain bc : blockChains) {
+            if (hash == bc.getHash()) {
                 return bc;
             }
         }
         return null;
     }
-    public static String printBlockChain(BlockChain bc){
+
+    public static String printBlockChain(BlockChain bc) {
         return bc.printBlocks();
+    }
+
+    public static boolean replaceBlockChain(BlockChain newBC, String hash) {
+        for (int i = 0; i < blockChains.size(); i++){
+            if (hash == blockChains.get(i).getHash()) {
+                blockChains.set(i, newBC);
+                return true;
+            }
+        }
+        return false;
     }
 }
